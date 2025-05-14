3673f9916cf6d498059529b0ac7a866a687b2cc6
hyna <bylo@duck.com>
2025-05-15 01:06:26 +0200

    feat(function): challenge => multiple return values short way

--- fuctions.go
+++ fuctions.go
@@ -6,12 +6,15 @@ func Greeting(name *string) {
 	println("In greeting: Hi again", *name)
 }
 
-//challenge
+// challenge
 func SumProductDiff(i, j int) (s int, p int, d int) { // named version
 
-	s = i + j
-	p = i * j
-	d = i - j
+	// s = i + j
+	// p = i * j
+	// d = i - j
 
-	return s, p, d
+	// return s, p, d
+
+	s, p, d = i+j, i*j, i-j
+	return
 }
