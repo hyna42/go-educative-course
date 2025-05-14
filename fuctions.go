f8ca3b1a9e2574e8be1c8cc3c907ddc00241f1ba
hyna <bylo@duck.com>
2025-05-15 01:04:18 +0200

    feat(function): challenge => multiple return values

--- fuctions.go
+++ fuctions.go
@@ -5,3 +5,13 @@ func Greeting(name *string) {
 	*name = "Johnny"
 	println("In greeting: Hi again", *name)
 }
+
+//challenge
+func SumProductDiff(i, j int) (s int, p int, d int) { // named version
+
+	s = i + j
+	p = i * j
+	d = i - j
+
+	return s, p, d
+}
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	name := "amadou"
 	Greeting(&name)
+
+	//challenge functon : multiple return
+	var s,p,d int = SumProductDiff(3, 4)
+
+	fmt.Printf("Sum : %d, Prod : %d, Diff : %d ",s,p,d)
+
 }
