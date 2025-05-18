3f43b5358a0bcfb6c49ae237f3341650d41b8cc0
hyna <bylo@duck.com>
2025-05-18 16:23:23 +0200

    feat(challenge):filter even and odd numbers

--- fuctions.go
+++ fuctions.go
@@ -37,3 +37,25 @@ func MyFactorial(n uint64) (fac uint64) {
 	}
 	return
 }
+
+//challenge : filter even and odd numbers
+
+type flt func(int) bool // aliasing type
+
+func IsEven(n int) bool { // check if n is even or not
+	return n%2 == 0
+}
+
+func Filter(sl []int, f flt) (yes, no []int) { // split s into two slices: even and odd
+
+	for _, n := range sl {
+		if f(n) {
+			yes = append(yes, n)
+		} else {
+
+			no = append(no, n)
+		}
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -7,16 +7,24 @@ func main() {
 	Greeting(&name)
 
 	//challenge functon : multiple return
-	var s,p,d int = SumProductDiff(3, 4)
+	var s, p, d int = SumProductDiff(3, 4)
 
-	fmt.Printf("Sum : %d, Prod : %d, Diff : %d\n ",s,p,d)
+	fmt.Printf("Sum : %d, Prod : %d, Diff : %d\n ", s, p, d)
 
-	sum := SumInts(1,2,3,4,888)
+	sum := SumInts(1, 2, 3, 4, 888)
 	//challenge sumInts
-	fmt.Printf("Sum 2: %d\n",sum)
+	fmt.Printf("Sum 2: %d\n", sum)
 
 	//challenge factoriel
 	fac := MyFactorial(10)
-	fmt.Printf("Factorielle : %d\n",fac)
+	fmt.Printf("Factorielle : %d\n", fac)
+
+	//challenge Filter Even and Odd Numbers
+	sl := []int{1, 2, 3, 4, 5, 7}
+
+	even, odd := Filter(sl, IsEven)
+
+	fmt.Printf("even : %d\n", even)
+	fmt.Printf("odd : %d\n", odd)
 
 }
