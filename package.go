6534a76401a9511dbb0fbece5c9677f71f7e95c8
hyna <bylo@duck.com>
2025-06-02 21:45:13 +0200

    feat(package):challenge 1

---
+++ package.go
@@ -0,0 +1,10 @@
+package main
+
+import "container/list"
+
+// Challenge: Construct a Double-Linked List
+func InsertListElements(n int)(*list.List){   // add elements in list from 1 to n
+  lst := list.New()
+
+  return lst
+}
