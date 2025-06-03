9f5435ed734ba6cf96aa7f1799d779a4869f6926
hyna <bylo@duck.com>
2025-06-03 23:58:43 +0200

    feat(struct):challenge 2, starting

--- struct.go
+++ struct.go
@@ -2,27 +2,23 @@ package main
 
 //Challenge: Anonymous Struct
 
- type S struct{
+type S struct {
 	a float32
-	int 
+	int
 	string
-
- }
-
+}
 
 // Challenge: Coordinates of a Point
-type Point struct { 
+type Point struct {
 	X, Y float64
 }
 
-func (p *Point) Abs() float64{
+func (p *Point) Abs() float64 {
 
-
-	return 1;
+	return 1
 }
 
-func (p *Point) Scale() float64{
+func (p *Point) Scale(s float64) {
 
-
-	return 1;
+	return
 }
