873342477bea62ee297448440f9401041b5bb06f
hyna <bylo@duck.com>
2025-06-03 23:56:51 +0200

    feat(struct):challenge 2, starting

--- struct.go
+++ struct.go
@@ -10,3 +10,19 @@ package main
  }
 
 
+// Challenge: Coordinates of a Point
+type Point struct { 
+	X, Y float64
+}
+
+func (p *Point) Abs() float64{
+
+
+	return 1;
+}
+
+func (p *Point) Scale() float64{
+
+
+	return 1;
+}
