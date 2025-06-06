b54808e8eb7418bcd9c92a77402cee1c783fde87
hyna <bylo@duck.com>
2025-06-06 23:58:36 +0200

    feat(interface): challeng 2"

--- interface.go
+++ interface.go
@@ -26,3 +26,28 @@ func Operate(s Simpler) {
 }
 
 //Challenge: Advancing the Simple Interface
+type RSimple struct {
+	r int
+}
+
+func (r *RSimple) Get() int {
+	return r.r
+}
+
+func (r *RSimple) Set(v int) {
+	r.r = v
+}
+
+func ROperate(s Simpler) {
+	s.Set(42)
+	fmt.Println("Value:", s.Get())
+
+	switch v := s.(type) {
+	case *Simple:
+		fmt.Println("Handled a Simple struct:", v.v)
+	case *RSimple:
+		fmt.Println("Handled a RSimple struct:", v.r)
+	default:
+		fmt.Println("Unknown Simpler type")
+	}
+}
