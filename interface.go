d7f5e695fdfbdcbb9132fc5656fd3e9a5b433096
hyna <bylo@duck.com>
2025-06-06 23:16:07 +0200

    feat(interface): challenge 1

--- interface.go
+++ interface.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+// Challenge: Make a Simple Interface
+type Simpler interface {
+	Get() int
+	Set(p int)
+}
+
+type Simple struct {
+	v int
+}
+
+
+func (s *Simple) Get() int {
+	return s.v
+}
+
+func (s *Simple) Set(u int) {
+	s.v = u
+}
+
+func operate(s Simpler) {
+	s.Set(42)
+	fmt.Println("Value:", s.Get())
+}
