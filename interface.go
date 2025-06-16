3d6c778cd2d0810b0bfb2efb56c6f6da5f43967b
hyna <bylo@duck.com>
2025-06-16 23:41:36 +0200

    feat(interface): challenge 3 algo

--- interface.go
+++ interface.go
@@ -53,3 +53,31 @@ func ROperate(s Simpler) {
 }
 
 // Challenge: Advancing the Shapes Analysis
+type Square struct {
+	side float32
+}
+
+type Triangle struct { // implement this struct
+	base   float32
+	height float32
+}
+
+type AreaInterface interface {
+	Area() float32
+}
+
+type PeriInterface interface { // implement this interface
+	Perimeter() float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func (sq *Square) Perimeter() float32 { // implement method called on square to calculate its perimeter
+	return sq.side * 4
+}
+
+func (tr *Triangle) Area() float32 { // implement method called on triangle to calculate its area
+	return 0.5 * (tr.base * tr.height)
+}
