946d1cd5d3264be908f07191395ad5d56f5e977c
hyna <bylo@duck.com>
2025-06-17 23:43:14 +0200

    feat(w/r): write and read with bufio

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	//Reading & Writing using Scanln /  Sscan-family
 	var (
-		firstName, lastName, s string
-		i                      int
-		f                      float32
-		input                  = "56.12 / 5212 / Go"
-		format                 = "%f / %d / %s"
+		firstName, lastName string
+		// i                      int
+		// f                      float32
+		// input1                 = "56.12 / 5212 / Go"
+		// format                 = "%f / %d / %s"
 	)
 
 	fmt.Println("Please enter your full name: ")
 	fmt.Scanln(&firstName, &lastName)
-	fmt.Printf("Hi %s %s!\n", firstName, lastName) 
-	fmt.Sscanf(input, format, &f, &i, &s)
-	fmt.Println("From the string we read: ", f, i, s)
+	fmt.Printf("Hi you %s %s!\n", firstName, lastName)
+	// fmt.Sscanf(input1, format, &f, &i, &s)
+	// fmt.Println("From the string we read: ", f, i, s)
+
+	//Reading & Writing using bufio
+	var inputReader *bufio.Reader
+	var (
+		input2 string
+		err    error
+	)
+	inputReader = bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter some input: ")
+
+	input2, err = inputReader.ReadString('\n')
+
+	if err == nil {
+		fmt.Printf("Hi you again : %s\n", input2)
+	}
+
 }
