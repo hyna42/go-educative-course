8e5bc6f80736ec5263d8fa5194e4b58728728582
hyna <bylo@duck.com>
2025-06-19 00:32:19 +0200

    feat(readFile)

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"io"
+// 	"os"
+// )
 
 func main() {
 	//Reading & Writing using Scanln /  Sscan-family
@@ -32,22 +32,29 @@ func main() {
 	// 	fmt.Printf("Hi you again : %s\n", input2)
 	// }
 	//challenge 1 : read and write
-	inputReader := bufio.NewReader(os.Stdin)
-	fmt.Println("Please enter some input, type S in the new line to stop: ")
-	for {
-		input, err := inputReader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("An error occurred: %s\n", err)
-			return
-		}
-		if strings.TrimSpace(input) == "S" { 
-			fmt.Println("Here are the counts:")
-			fmt.Printf("Number of characters: %d\n", nrchars)
-			fmt.Printf("Number of words: %d\n", nrwords)
-			fmt.Printf("Number of lines: %d\n", nrlines)
-			os.Exit(0)
-		}
-		Counters(input)
-	}
+	// inputReader := bufio.NewReader(os.Stdin)
+	// fmt.Println("Please enter some input, type S in the new line to stop: ")
+	// for {
+	// 	input, err := inputReader.ReadString('\n')
+	// 	if err != nil {
+	// 		fmt.Printf("An error occurred: %s\n", err)
+	// 		return
+	// 	}
+	// 	if strings.TrimSpace(input) == "S" {
+	// 		fmt.Println("Here are the counts:")
+	// 		fmt.Printf("Number of characters: %d\n", nrchars)
+	// 		fmt.Printf("Number of words: %d\n", nrwords)
+	// 		fmt.Printf("Number of lines: %d\n", nrlines)
+	// 		os.Exit(0)
+	// 	}
+	// 	Counters(input)
+	// }
+
+	//reading file
+	
+	//reading with readfile
+	var file = "read.txt"
+	// RwithBufferIo(file)
+	// RwithReadFile(file)
 
 }
--- rw.go
+++ rw.go
@@ -1,11 +1,46 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 var nrchars, nrwords, nrlines = 0, 0, 0
 
 func Counters(input string) {
-	nrchars += len(input) - 2 
+	nrchars += len(input) - 2
 	nrwords += strings.Count(input, " ") + 1
 	nrlines++
+}
+
+// methode 1 :with buffor.io
+func RwithBufferIo(file string) {
+	inputFile, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Error on reading file", err)
+		return
+	}
+	defer inputFile.Close()
+	reader := bufio.NewReader(inputFile)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Println("R BufferIO ==>",line)
+			break
+		}
+		fmt.Println("Erreur :", err)
+	}
+
+}
+
+// methode 2 readfile
+func RwithReadFile(file string) {
+	content, er := os.ReadFile(file)
+	if er != nil {
+		fmt.Println("Error on reading file")
+	}
+	fmt.Println("R ReadFile ==>", string(content))
 }
